2dc30708207c9a182f578366845af0d0622f6cf8
Priszi <h163001@stud.u-szeged.hu>
2023-04-04 13:28:19 +0200

    Created basic discord bot

---
+++ WeatherBot.py
@@ -0,0 +1,44 @@
+import pathlib
+
+import discord
+import pathlib
+import json
+
+
+def get_token(filename="discord-token.json"):
+    """
+        :param filename: The name of the token configuration file.
+        :type filename: str
+        :return: The token that is used to run the discord client.
+        :rtype: str
+    """
+    path = pathlib.Path.cwd().joinpath(filename).__str__()
+    token = None
+    with open(path, "r") as f:
+        token = json.load(f)['token']
+    return token
+
+
+TOKEN = get_token()
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'Connection initiated with discord client: {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('Are you up?') or message.content.contains('up'):
+        await message.channel.send("Yes, I'm online and ready to go!")
+
+
+client.run(TOKEN)
